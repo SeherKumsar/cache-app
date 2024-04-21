5db92daaa76535e8c5e6c8f44b3d05d925a861f1
SeherKumsar <seherkumsar99@outlook.com>
2024-04-22 00:34:27 +0300

    changed project

--- app/helper/mysql.js
+++ app/helper/mysql.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   connectionLimit: 100, // 100 tane bağlantı oluşturabilir
-  host: "10.244.0.236",
+  host: "10.99.134.202",
   user: "root",
-  port: "3306",
+  port: "3310",
   password: "password",
   database: "app",
 });
--- app/index.js
+++ app/index.js
@@ -1,170 +1,51 @@
 const express = require("express");
-// const redis = require("redis");
-const dbConnection = require("./helper/mysql");
+const { createPool } = require("mysql2/promise");
+const redis = require("redis");
 const app = express();
-
-// const redisClient = redis.createClient({
-//     host: "redis://redis-service:6379",
-//     port: 6379,
-// });
-
-// const redisClient = redis.createClient({
-//   url: "redis://10.244.0.234:6379",
-// });
-
-// redisClient.on('error', (err) => console.log('Redis Client Error', err));
-// redisClient.connect();
-// app.get('/', async (req, res) => {
-//     const key = "counter";
-//     try {
-//         let counter = await redisClient.getAsync(key);
-//         res.send(`Counter: ${counter}`);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-// redisClient.on("error", (err) => console.log("Redis Client Error", err));
-
-// (async () => {
-//   try {
-//     await redisClient.connect();
-//     console.log("Connected to Redis server");
-//   } catch (error) {
-//     console.error("Error connecting to Redis:", error);
-//   }
-// })();
-
-app.get("/students", (req, res) => {
-  dbConnection.query("SELECT * FROM students", (err, results, fields) => {
-    if (err) {
-      console.log("Database query error: ", err);
+const pool = createPool({
+  host: "10.99.134.202",
+  user: "root",
+  password: "password",
+  database: "app",
+  port: 3310,
+});
+// Redis connection
+const redisClient = redis.createClient({
+  url: "redis://10.96.253.63:6379",
+});
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log("Connected to Redis server");
+  } catch (error) {
+    console.error("Error connecting to Redis:", error);
+  }
+})();
+app.get("/students/:id", async (req, res) => {
+  const key = req.params.id;
+  try {
+    // Retrieve data from Redis with error handling
+    const cachedData = await redisClient.get(key);
+    if (cachedData) {
+      console.log("Data retrieved from Redis cache");
+      return res.json(JSON.parse(cachedData));
+    }
+    const [rows] = await pool.query("SELECT * FROM students WHERE id = ?", [
+      key,
+    ]);
+    if (rows.length > 0) {
+      const rowData = rows[0];
+      await redisClient.set(key, JSON.stringify(rowData));
+      console.log("Data cached in Redis");
+      return res.json(rowData);
     } else {
-      res.status(200).json({
-        status: "success",
-        data: results,
-      });
+      return res.status(404).send("Data not found");
     }
-  });
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
-
-app.post("/students/add/:name/:age/:midterm_grade/:final_grade", (req, res) => {
-  dbConnection.query(
-    "INSERT INTO students (name, age, midterm_grade, final_grade) VALUES (?, ?, ?, ?)",
-    [
-      req.params.name,
-      req.params.age,
-      req.params.midterm_grade,
-      req.params.final_grade,
-    ],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        res.status(200).json({
-          status: "success",
-          data: results,
-        });
-      }
-    }
-  );
-});
-
-app.get("/students/add/:name/:age/:midterm_grade/:final_grade", (req, res) => {
-  dbConnection.query(
-    "INSERT INTO students (name, age, midterm_grade, final_grade) VALUES (?, ?, ?, ?)",
-    [
-      req.params.name,
-      req.params.age,
-      req.params.midterm_grade,
-      req.params.final_grade,
-    ],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        res.status(200).json({
-          status: "success",
-          data: results,
-        });
-      }
-    }
-  );
-});
-
-app.delete("/students/delete/:id", (req, res) => {
-  dbConnection.query(
-    "DELETE FROM students WHERE id = ?",
-    [req.params.id],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        res.status(200).json({
-          status: "success",
-          data: results,
-        });
-      }
-    }
-  );
-});
-
-app.get("/students/delete/:id", (req, res) => {
-  dbConnection.query(
-    "DELETE FROM students WHERE id = ?",
-    [req.params.id],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        res.status(200).json({
-          status: "success",
-          data: results,
-        });
-      }
-    }
-  );
-});
-
-app.get("/students/average/:id", (req, res) => {
-  dbConnection.query(
-    "SELECT (midterm_grade + final_grade) / 2 AS average_grade FROM students WHERE id = ?",
-    [req.params.id],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        const averageGrade = parseFloat(results[0].average_grade).toFixed(2);
-        res.status(200).json({
-          status: "success",
-          // data: results,
-          data: {
-            average_grade: averageGrade,
-          },
-        });
-      }
-    }
-  );
-});
-
-app.get("/students/:id", (req, res) => {
-  dbConnection.query(
-    "SELECT * FROM students WHERE id = ?",
-    [req.params.id],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Database query error: ", err);
-      } else {
-        res.status(200).json({
-          status: "success",
-          data: results,
-        });
-      }
-    }
-  );
-});
-
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port 3000`);
 });
